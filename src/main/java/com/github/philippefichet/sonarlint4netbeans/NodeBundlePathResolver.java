83db13afad61a20274f7402c18e42e2e38088369
FICHET Philippe <philippe.fichet@laposte.net>
2022-02-26 17:36:10 +0100

    NodeBundlePathResolver search env vars

--- src/main/java/com/github/philippefichet/sonarlint4netbeans/NodeBundlePathResolver.java
+++ src/main/java/com/github/philippefichet/sonarlint4netbeans/NodeBundlePathResolver.java
@@ -1,6 +1,21 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * sonarlint4netbeans: SonarLint integration for Apache Netbeans
+ * Copyright (C) 2020 Philippe FICHET.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
  */
 package com.github.philippefichet.sonarlint4netbeans;
 
@@ -15,6 +30,8 @@ public class NodeBundlePathResolver implements BundlePathResolver
     @Override
     public String resolve(String relativePath) {
         System.out.println("relativePath = " + relativePath);
+        System.out.println("System.getenv() = " + System.getenv());
+        System.out.println("System.getenv().get(\"PATH\") = " + System.getenv().get("PATH"));
         return null;
     }
     
